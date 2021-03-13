b7f3b9d5995c2f011cf299d2d31956c36ae4a587
shreyasmt <smtshrews@gmail.com>
2021-03-13 19:55:11 +1000

    ternary added

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
 var gradient = document.getElementById("gradient");
-var option;
+css.textContent = "";
 function linear(){
     body.style.background = "linear-gradient(to right, "+ color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background;   
+    css.textContent = body.style.background;  
 }
 function radial(){
     body.style.background = "radial-gradient(circle at center," + color1.value + "," + color2.value +")";
-    css.textContent = body.style.background;
+    css.textContent = body.style.background; 
 }
 
+function lineargradient(x){
+    console.log(x);
+    color1.addEventListener('input',linear,true);
+    color2.addEventListener('input',linear,true);
+}
 
+function radialgradient(x){
+    console.log(x);
+    color1.addEventListener('input',radial,true);
+    color2.addEventListener('input',radial,true);
+}
 
 gradient.onchange = function() {
     var x = document.getElementById("gradient").value;
-    
-        if(x == "radial"){
-            console.log(x);
-            color1.addEventListener('input',radial,true);
-            color2.addEventListener('input',radial,true);
-        }else if(x == "linear"){
-            console.log(x);
-            color1.addEventListener('input',linear,true);
-            color2.addEventListener('input',linear,true);
-        }
-
+    (x == "radial") ? radialgradient(x) : lineargradient(x);
 }
 
 
